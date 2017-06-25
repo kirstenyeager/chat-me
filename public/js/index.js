9675944b2c98f49bf8affb7b159cf87771b0332c
Kirsten Yeager <kirsten@yscpas.com>
2017-06-25 11:16:07 -0400

    fixed

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+
+var socket = io();
+
+socket.on('connect', function() {
+	console.log('connect to server');
+
+});
+socket.on('disconnect', function() {
+	console.log('Disconnected from server.');
+});
+
+socket.on('newMessage', function(message) {
+	console.log('new message', message);
+});
+
+
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const publicPath = path.join('__dirname', '../public');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3001;
 var app = express();
 
@@ -14,17 +14,23 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
 	console.log('New user connected');
+
 	
+	socket.on('createMessage', function(message) {
+		console.log('Received Message:', message );
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		});
+	});
+	
+
 	socket.on('disconnect', () => {
 		console.log('User disconnected');
 	});
 });
 
-
-
-// app.get('/', () => {
-// 	console.log('hello');
-// })
 
 server.listen(port, () => {
 	console.log(`listening on ${port}`);
