ec00f6cd9ffe07260899e1d9a37df5c5ac70ac9f
Kirsten Yeager <kirsten@yscpas.com>
2017-07-01 22:57:02 -0400

    added form

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,3 @@
-const {generateMessage} = require('./utils/message');
 var socket = io();
 
 socket.on('connect', function () {
@@ -11,6 +10,26 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage', generateMessage('user1', 'hello there'));
+socket.emit('createMessage', {
+	from: 'Frank',
+	text: 'Hi'
+}, function (data) {
+	console.log('Got it', data);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+	e.preventDefault();
+
+	socket.emit('createMessage', {
+		from: 'User',
+		text: jQuery('[name=message]').val()
+	}, function () {
+
+	});
+});
